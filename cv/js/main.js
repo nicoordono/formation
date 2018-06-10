46639df2535a43248f405795d522e3bea02a3854
niconwo <38044304+niconwo@users.noreply.github.com>
2018-06-10 22:24:34 +0200

    Add files via upload

--- cv/js/main.js
+++ cv/js/main.js
@@ -1,1 +1,12 @@
-document.getElementsByClassName('active').style.color= "purple";
+document.getElementByname("active").style.color = "red";
+var i = 0;
+var txt = 'nicolas ordono developpeur débutant expert en bureautique';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("h1").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
