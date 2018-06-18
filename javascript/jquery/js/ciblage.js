fafae1e225f12e04cd02aba8d02948573ce68716
niconwo <38044304+niconwo@users.noreply.github.com>
2018-06-18 11:54:14 +0200

    Add files via upload

---
+++ javascript/jquery/js/ciblage.js
@@ -0,0 +1,8 @@
+$(function(){
+
+    $('span:first,span:last').css('color', 'red');
+ 
+});
+//document.querySelectorAll('span')[0].style.color="red";
+//document.querySelectorAll('span')[3].style.color="red";
+
