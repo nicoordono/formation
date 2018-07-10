7bc4a362d781f00de5c997d04220973b974d4baf
niconwo <38044304+niconwo@users.noreply.github.com>
2018-07-10 09:59:58 +0200

    Add files via upload

---
+++ php/variables/TabAso/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $activites = array("Lundi"=>"Php","Mardi"=>"Javascript", "Mercredi"=>"Typescript","Jeudi"=>"Angular","Vendredi"=>"SQL","Samedi"=>"Python","Dimanche"=>"Tout");
+    //deux dimensions
+    $pierre = array("1"=>"Php","2"=>"Javascript","3"=>"Python");
+    $paul = array("1"=>"Javascript","2"=>"Css","3"=>"Php");
+    $jacques = array("1"=>"Java","2"=>"Php","3"=>"Javascript");
+    $preferences = array();
+    var_dump($activites);
+    ?>
+</body>
+</html>
