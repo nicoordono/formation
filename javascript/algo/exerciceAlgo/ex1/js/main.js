52ae59a73ed921763991107198df4dd8c005a352
niconwo <38044304+niconwo@users.noreply.github.com>
2018-07-12 11:45:59 +0200

    Add files via upload

---
+++ javascript/algo/exerciceAlgo/ex1/js/main.js
@@ -0,0 +1,29 @@
+var identifiant = prompt ('ton login');
+var motdepasse = prompt('ton mot de passe');
+var compteur = 5;
+if (identifiant == 'hello' && motdepasse == '64')for (pas = 0; pas < 5; pas++)  {
+  alert('le login bon et mot de passe bon')
+}
+else if(identifiant != 'hello' && motdepasse != '64') {
+    var identifiant = prompt ('ton login');
+var motdepasse = prompt('ton mot de passe');
+
+alert('pas bon')
+} 
+//for (var compteur = 5; identifiant == 'hello' && motdepasse == '64';){
+ //alert( alert('le login bon et mot de passe bon'))
+//}
+//while ( identifiant != 'hello' && motdepasse != '64') {
+  //  prompt >5;
+//}
+
+
+//while (identifiant == 'hello' && motdepasse == '64'){
+  //  alert ('le login bon et le mot de passe bon ')
+//if (identifiant != 'hello' && motdepasse != '64') {
+  //  var identifiant = prompt ('ton login');
+    //var motdepasse = prompt ('ton mot de passe');  
+    //continue
+  
+//}
+//identifiant == 'hello' && motdepasse == '64';
